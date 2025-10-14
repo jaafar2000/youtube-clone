5a5dbb154c2ed8075b08468b08aeaec56994cf8e
jaafar yousssef <103080371+jaafar2000@users.noreply.github.com>
2025-10-15 01:37:37 +0400

    Update Feed.js

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -20,7 +20,7 @@ const Feed = () => {
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2022 JSM Media
+          Copyright © 2022 JHY Media
         </Typography>
       </Box>
 
